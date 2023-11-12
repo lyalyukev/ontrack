d3bf01bc62e095de7a434fbdf68133becd147b3b
Lialiuk Yevhen <lyalyuk.ev@gmail.com>
2023-11-12 16:55:02 +0200

    Imrovement calculate total time 60

--- src/components/ActivitySecondsToComplete.vue
+++ src/components/ActivitySecondsToComplete.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {formatSecond, getTotalActivitySeconds} from "@/function";
 import {isActivityValid, validateTimelineItems} from "@/validators";
+import {computed} from "vue";
 
 const props = defineProps({
   activity: {
@@ -15,12 +16,14 @@ const props = defineProps({
   }
 })
 
-const seconds = formatSecond(getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete)
+const colorClasses = computed(() => secondsDiff.value < 0 ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600')
+const seconds = computed(() => `${sign.value}${formatSecond(secondsDiff.value)}`)
+const sign = computed(() => secondsDiff.value >= 0 ? '+' : '-')
+const secondsDiff = computed(() => getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete)
+const classes = `flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600 ${colorClasses.value}`
 
 </script>
 
 <template>
-  <div class="flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600">
-    {{ formatSecond(activity.secondsToComplete) }}
-  </div>
+  <div :class="classes">{{ seconds }}</div>
 </template>
